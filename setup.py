7b16835e24002a24a50d78e2de23d8f3c6555f7c
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-08-27 13:26:48 +0200

    with web socket handling

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+setup(
+    name='bluenaas_capabilities',
+    version='0.0.1',
+    author='Shailesh Appukuttan',
+    author_email='shailesh.appukuttan@unic.cnrs-gif.fr',
+    package_data={'basalunit': json_files},
+    url='https://github.com/appukuttan-shailesh/bluenaas_capabilities/',
+    keywords = ['BlueNaaS', 'SciUnit', 'NEURON', 'python', 'validation framework'],
+    license='MIT',
+    description='A SciUnit library for handling NEURON-Python models having different internal implementations/formats.',
+    long_description="",
+    install_requires=['sciunit>=0.2.1.1', 'neuron'],
+    py_modules=["bluenaas_capabilities"]
+)
